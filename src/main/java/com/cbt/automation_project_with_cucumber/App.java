d8e202a965da54175b722d966735b820390eaeb0
freddy123456 <sheenobee@inbox.ru>
2016-12-11 14:13:51 -0500

    initial commit

---
+++ src/main/java/com/cbt/automation_project_with_cucumber/App.java
@@ -0,0 +1,13 @@
+package com.cbt.automation_project_with_cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
